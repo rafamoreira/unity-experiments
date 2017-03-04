9702f56d9bd4e3a18e87bbcc939cccba85b5a47d
rafael moreira <me@rafaelmoreira.net>
2017-03-04 20:17:28 -0300

    Add Hexes and Meshese

---
+++ Hexes/Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class HexMetrics{
+
+    public const float outerRadius = 10f;
+
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+    public static Vector3[] corners =
+    {
+        new Vector3(0, 0, outerRadius),
+        new Vector3(innerRadius, 0, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0, -0.5f * outerRadius),
+        new Vector3(0, 0, -outerRadius),
+        new Vector3(-innerRadius, 0, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0, 0.5f * outerRadius)
+    };
+}
---
+++ Meshes/Assets/Rounded Cubes/Cube.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Cube : MonoBehaviour
+{
+
+    public int xSize, ySize, zSize;
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Cube";
+        CreateVertices();
+        CreateTriangles();
+    }
+
+    private void CreateVertices()
+    {
+        int cornerVertices = 8;
+        int edgeVertices = (xSize + ySize + zSize - 3) * 4;
+        int faceVertices = ((xSize - 1) * (ySize - 1) + (xSize - 1) * (zSize - 1) + (ySize - 1) * (zSize - 1)) * 2;
+        vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+        int v = 0;
+        for (int y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, y, 0);
+            }
+            for (int z = 1; z <= zSize; z++)
+            {
+                vertices[v++] = new Vector3(xSize, y, z);
+            }
+            for (int x = xSize - 1; x >= 0; x--)
+            {
+                vertices[v++] = new Vector3(x, y, zSize);
+            }
+            for (int z = zSize - 1; z > 0; z--)
+            {
+                vertices[v++] = new Vector3(0, y, z);
+            }
+        }
+        for (int z = 1; z < zSize; z++)
+        {
+            for (int x = 1; x < xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, ySize, z);
+            }
+        }
+        for (int z = 1; z < zSize; z++)
+        {
+            for (int x = 1; x < xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, 0, z);
+            }
+        }
+    }
+
+    private void CreateTriangles()
+    {
+        int quads = (xSize * ySize + xSize * zSize + ySize * zSize) * 2;
+        int[] triangles = new int[quads * 6];
+
+        int ring = (xSize + zSize) * 2;
+        int t = 0, v = 0;
+
+        for (int q = 0; q < xSize; q++, v++)
+        {
+            t = SetQuad(triangles, t, v, v + 1, v + ring, v + ring + 1);
+        }
+
+        mesh.triangles = triangles;
+    }
+
+    private static int SetQuad(int[] triangles, int i, int v00, int v10, int v01, int v11)
+    {
+        triangles[i] = v00;
+        triangles[i + 1] = triangles[i + 4] = v01;
+        triangles[i + 2] = triangles[i + 3] = v10;
+        triangles[i + 5] = v11;
+        return i + 6;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
